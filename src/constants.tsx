a0744270c03a8cc89c9757da29e254a2846a14eb
Carlos Filoteo <c.filoteo12@gmail.com>
2024-06-22 13:14:57 -0600

    Scaffold

---
+++ src/constants.tsx
@@ -0,0 +1,13 @@
+export const KeyCode = Object.freeze({
+  ENTER: 'Enter',
+  SPACE: ' ',
+  ESC: 'Escape',
+  PAGE_UP: 'PageUp',
+  PAGE_DOWN: 'PageDown',
+  HOME: 'Home',
+  END: 'End',
+  UP: 'ArrowUp',
+  DOWN: 'ArrowDown',
+  RIGHT: 'ArrowRight',
+  LEFT: 'ArrowLeft',
+})

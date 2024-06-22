ca4e525080584c74f46aaa99e6a06366345abdb3
Carlos Filoteo <c.filoteo12@gmail.com>
2024-06-22 17:37:13 -0600

    Latest TypeScript types for Popover API

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+/* below enables Popover API typings - @see https://github.com/DefinitelyTyped/DefinitelyTyped/pull/69670 */
+/// <reference types="react/canary" />

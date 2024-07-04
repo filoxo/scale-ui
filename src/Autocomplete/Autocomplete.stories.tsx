13b9aa36f7996b2c093d2102058e3fcefbd886c6
Carlos Filoteo <c.filoteo12@gmail.com>
2024-07-04 17:24:30 -0600

    Replace invalid className

--- src/Autocomplete/Autocomplete.stories.tsx
+++ src/Autocomplete/Autocomplete.stories.tsx
@@ -13,19 +13,19 @@ export default {
 export const Default: Story<AutocompleteProps> = () => {
   const [displayValue, setDisplayValue] = useState("");
   return (
-    <div className="min-h-250px">
+    <div style={{ minHeight: 250 }}>
       <label htmlFor="demo">Select an option</label>
       <Autocomplete
         id="demo"
         value={displayValue}
         onChange={(e) => {
-          setDisplayValue(e.target.value);
+          setDisplayValue(e.target.value)
         }}
         onOptionSelect={(option: unknown) => {
           const value =
-            typeof option === "string" ? option : JSON.stringify(option);
-          setDisplayValue(value);
-          return false; // this controls whether the autocomplete stays open or not after clicking on an option
+            typeof option === "string" ? option : JSON.stringify(option)
+          setDisplayValue(value)
+          return false // this controls whether the autocomplete stays open or not after clicking on an option
         }}
       >
         <AutocompleteOption value="A" />
@@ -43,5 +43,5 @@ export const Default: Story<AutocompleteProps> = () => {
         </AutocompleteOption>
       </Autocomplete>
     </div>
-  );
+  )
 };

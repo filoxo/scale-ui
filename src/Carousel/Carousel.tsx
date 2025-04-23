8d84a4a22a0ae440013fadc2e83f5624903e225c
Carlos Filoteo <c.filoteo12@gmail.com>
2025-04-23 17:42:39 -0600

    Fix jump on mobile devices

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -4,12 +4,12 @@ import "./carousel.css"
 export interface CarouselProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const Carousel = forwardRef<HTMLDivElement, CarouselProps>(
-  ({ className, ...props }, ref) => {
+  ({ className = "", ...props }, ref) => {
     return <div {...props} className={`carousel ${className}`} ref={ref}></div>
   }
 )
 
-export function CarouselItem({ className, ...props }: CarouselProps) {
+export function CarouselItem({ className = "", ...props }: CarouselProps) {
   return <div className={`carousel-item ${className}`} {...props}></div>
 }
 
@@ -43,7 +43,10 @@ export const useCarouselControls = () => {
     const visibleItems = items.map((item) => isInView(containerRef, item))
     const lastVisibleIndex = visibleItems.indexOf(true)
     const prevSlide = items?.[lastVisibleIndex - 1] || items?.[items.length - 1]
-    prevSlide?.scrollIntoView({ behavior: "smooth" })
+    prevSlide?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+    })
   }
 
   function onNextClick() {
@@ -51,7 +54,10 @@ export const useCarouselControls = () => {
     const visibleItems = items.map((item) => isInView(containerRef, item))
     const lastVisibleIndex = visibleItems.lastIndexOf(true)
     const nextSlide = items?.[lastVisibleIndex + 1] || items?.[0]
-    nextSlide?.scrollIntoView({ behavior: "smooth" })
+    nextSlide?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+    })
   }
 
   return {
